61eb2861ca5b19bd130ff68686354f8f8dc5bae5
Stathis Xantinidis <spithash@tuta.io>
2023-06-20 22:38:29 +0300

    Added comments

--- iptables-rules.sh
+++ iptables-rules.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+#Block Invalid Packets
 iptables -t mangle -A PREROUTING -m conntrack --ctstate INVALID -j DROP
+
+#Block New Packets That Are Not SYN
 iptables -t mangle -A PREROUTING -p tcp ! --syn -m conntrack --ctstate NEW -j DROP
+
+#Block Uncommon MSS Values
 iptables -t mangle -A PREROUTING -p tcp -m conntrack --ctstate NEW -m tcpmss ! --mss 536:65535 -j DROP
+
+#Block Packets With Bogus TCP Flags
 iptables -t mangle -A PREROUTING -p tcp --tcp-flags FIN,SYN FIN,SYN -j DROP
 iptables -t mangle -A PREROUTING -p tcp --tcp-flags SYN,RST SYN,RST -j DROP
 iptables -t mangle -A PREROUTING -p tcp --tcp-flags FIN,RST FIN,RST -j DROP
@@ -10,6 +17,8 @@ iptables -t mangle -A PREROUTING -p tcp --tcp-flags FIN,ACK FIN -j DROP
 iptables -t mangle -A PREROUTING -p tcp --tcp-flags ACK,URG URG -j DROP
 iptables -t mangle -A PREROUTING -p tcp --tcp-flags ACK,PSH PSH -j DROP
 iptables -t mangle -A PREROUTING -p tcp --tcp-flags ALL NONE -j DROP
+
+#Block Packets From Private Subnets (Spoofing)
 iptables -t mangle -A PREROUTING -s 224.0.0.0/3 -j DROP
 iptables -t mangle -A PREROUTING -s 169.254.0.0/16 -j DROP
 iptables -t mangle -A PREROUTING -s 172.16.0.0/12 -j DROP
@@ -19,6 +28,12 @@ iptables -t mangle -A PREROUTING -s 10.0.0.0/8 -j DROP
 iptables -t mangle -A PREROUTING -s 0.0.0.0/8 -j DROP
 iptables -t mangle -A PREROUTING -s 240.0.0.0/5 -j DROP
 iptables -t mangle -A PREROUTING -s 127.0.0.0/8 ! -i lo -j DROP
+
+#Drop all ICMP packets
 iptables -t mangle -A PREROUTING -p icmp -j DROP
+
+#Reject connections from hosts that have more than 80 established connections
 iptables -A INPUT -p tcp -m connlimit --connlimit-above 80 -j REJECT --reject-with tcp-reset
+
+#Block fragmented packets
 iptables -t mangle -A PREROUTING -f -j DROP
